a9a2832615f1dddc6155f1ae9038b20096393462
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-06 15:55:08 +0300

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-         int n1 = 0, n2 = 1, n3, i, count = 10;
+         int n1 = 0, n2 = 1, n3, i, count;
+         Scanner input = new Scanner(System.in);
+         System.out.print("How many digits: ");
+         count = input.nextInt();
          System.out.print(n1 + " " + n2); 
-         for (i = 0; i < count; i++) {
+         for (i = 1; i < count; i++) {
                  n3 = n1 + n2;
                  System.out.print(" " + n3);
                  n1 = n2;
