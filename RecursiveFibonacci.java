eba49ffdec779693d5209cc75377c464af48f1a3
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-08 15:40:05 +0300

    Create RecursiveFibonacci.java
    Program to find fibonacci series with recursive methods in Java.

---
+++ RecursiveFibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PatikaDev {
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } 
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Number => ");
+        System.out.println(fib(s.nextInt()));
+    }
+}
