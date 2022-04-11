bf5aef4ea78e791037c4cdbe88cc3ad136b3f1e5
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-04-11 15:38:32 +0300

    Create PrintPattern.java

---
+++ PrintPattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrintPattern {
+  public static int pattern(int number) {
+    int original = number;
+    System.out.println(number);
+
+    if (number > 0) {
+      original = pattern(number -= 5);
+    }
+    if (number == 0 || number < 0) {
+      number +=5;
+      return number;
+    }
+    if (original > 0) {
+      number += 5;
+    }
+    System.out.println(number);
+    return number;
+  }
+  public static void main(String[] Args) {
+    
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the n number : ");
+    int n = s.nextInt();
+    System.out.print(pattern(n));
+  }
+}
